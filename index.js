8ef9be6d623e8c1d4f192e40ade4a07b74336091
Sam M <sammontalvojr@gmail.com>
2020-08-04 10:15:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+
+const moveDodgerLeft = () => {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+};
+
+const moveDodgerRight = () => {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  };
