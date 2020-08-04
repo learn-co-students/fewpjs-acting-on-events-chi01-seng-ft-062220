988e88166b0b1b5a9ccce1790f6624810674ccf4
embraman10@gmail.com <embraman10@gmail.com>
2020-08-04 13:45:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+
+// move left
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+// move right
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
